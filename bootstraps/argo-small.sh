be1006e317181dd59933f58640028a7343f7b6cc
dudick123 <brian.dudick@outlook.com>
2025-01-03 12:18:05 -0500

    test

--- bootstraps/argo-small.sh
+++ bootstraps/argo-small.sh
@@ -5,16 +5,26 @@ echo
 
 echo "Installing ArgoCD Using Helm"
 kubectl create namespace argocd
-helm repo add argo https://argoproj.github.io/argo-helm
-helm repo update
-helm repo list
-helm install argocd argo/argo-cd --version 6.7.11 --namespace argocd #--values ./helm/argocd/values.yaml
-helm list -n argocd
+# helm repo add argo https://argoproj.github.io/argo-helm
+# helm repo update
+# helm repo list
+# helm install argocd argo/argo-cd --version 6.7.11 --namespace argocd #--values ./helm/argocd/values.yaml
+# helm list -n argocd
+
+kubectl apply -n argocd -f ./manifests/kube.yaml
 
 echo "Setting the Argo Admin Password"
 export ARGO_ADMIN_PASSWORD=abc123
 kubectl patch secret -n argocd argocd-secret -p '{"stringData": { "admin.password": "'$(htpasswd -bnBC 10 "" $ARGO_ADMIN_PASSWORD | tr -d ':\n')'"}}'
 echo
+
+# echo "Patching the ArgoCD CM"
+# kubectl patch configmap argocd-cm -n argocd --patch '{
+#   "data": {
+#     "resource.customizations": "argoproj.io/v1alpha1,Application:\n  ignoreDifferences: |\n    jsonPointers:\n      - \"/metadata/annotations/foo.bar.com/snow-data\""
+#   }
+# }'
+# echo
 
 echo "Creating the ArgoCD Root Application"
 kubectl apply -f ./app-of-apps/root-app/my-application.yml
